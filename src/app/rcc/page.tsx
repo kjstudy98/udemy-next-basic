bc2f4ce7a73deca181b9d5f36a87b3cf1ebf213b
kjstudy-englife <kjstudy.englife@gmail.com>
2025-07-19 22:49:17 +0900

    Learned SSR/CSR

---
+++ src/app/rcc/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+
+export default function ClientComponent() {
+  console.log("Client");
+  return <div>ClientComponent</div>;
+}
---
+++ src/app/rsc/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function ServerComponent() {
+  console.log("Server");
+  return <div>ServerComponent</div>;
+}
